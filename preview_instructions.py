b61b4a68774257e06a561478d51139a536231b5d
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-09-18 14:02:08 +0100

    set cyan as reference color for new columns

--- preview_instructions.py
+++ preview_instructions.py
@@ -9,8 +9,8 @@ top = 5
 dataset = InstructionsDataset()
 
 table = Table(title=f"Instructions (first {top} records)", show_lines=True, box=box.DOUBLE_EDGE)
-table.add_column("Instruction")
-table.add_column("Output")
+table.add_column("Instruction", style="cyan")
+table.add_column("Output", style="cyan")
 
 for record in dataset.get_record():
     table.add_row(record["instruction"], record["output"])
